411c6e6a4ae6058275331b54dc6128fea8156495
Fergus Pitt <ferginspace@gmail.com>
2013-03-03 19:21:24 -0500

    Removed temporary working variables and un-needed defaults.

--- searchTweetForScores.py
+++ searchTweetForScores.py
@@ -1,10 +1,14 @@
-tweetText = "5:2 3:1 4:5 5/2 5-2 5:2"
-#tweetText = "5-2"
+#set up empty containers
 searchScore = ""
 runningTotal={}
+numberOfPredictions=0
 ii=0
+#count the number of times a score occurs, add it to a dictionary, update the number of predictions
 for i in listAllPerms[0]:
 	searchScore = ''.join(listAllPerms[0][ii])
-	if tweetText.count(searchScore)==1:
-		runningTotal[searchScore]=runningTotal[searchScore]+1
+	if finalString.count(searchScore) > 0:
+		runningTotal[searchScore]=finalString.count(searchScore)
+		numberOfPredictions=numberOfPredictions+finalString.count(searchScore)
 	ii=ii+1
+print runningTotal
+print numberOfPredictions
